09158677ff33b36cd08827ac6991af6844785c91
felttl <68735155+felttl@users.noreply.github.com>
2024-11-20 11:14:58 +0100

    update min functionality

---
+++ proj_ZenThéa/proj_ZenThéa/AccueilVC.swift
@@ -0,0 +1,28 @@
+//
+//  AccueilVC.swift
+//  proj_ZenThéa
+//
+//  Created by Felix Ton that-Lavarini on 20/11/2024.
+//
+
+import UIKit
+
+class AccueilVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    /// In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
