4b4b19baa086d1f6ce566824e5f8628d681d0c65
felttl <68735155+felttl@users.noreply.github.com>
2025-02-02 19:35:02 +0100

    add clean base

---
+++ ZenTheaSource/ZenTheaSource/MessagesMAVC.swift
@@ -0,0 +1,12 @@
+//
+//  MessagesMAVC.swift
+//  ZenTheaSource
+//
+//  Created by felix on 02/12/2024.
+//
+import UIKit
+import Messages
+
+class MessagesMAVC: MSMessagesAppViewController {
+
+}
---
+++ ZenTheaSource/ZenTheaSourceTests/ZenTheaSourceTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZenTheaSourceTests.swift
+//  ZenTheaSourceTests
+//
+//  Created by felix on 02/02/2025.
+//
+
+import Testing
+@testable import ZenTheaSource
+
+struct ZenTheaSourceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
